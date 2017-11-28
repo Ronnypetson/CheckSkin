8e5e565a7b9c658def7195d4ed0332b6a6f9c1cb
Ronnypetson <rsronnypetson4@gmail.com>
2017-11-28 19:08:14 -0300

    preprocessed data

--- divide_thumbs.py
+++ divide_thumbs.py
@@ -1,9 +1,11 @@
 from __future__ import print_function
 import os
 import json
+import cv2
 from random import randint
 from shutil import copyfile
 
+img_dim = 50
 json_fn = 'imgs_list.txt'
 
 jsonFile = open(json_fn,'r')
@@ -17,12 +19,16 @@ for v in values:
     dvalues = json.load(dfile)
     dfile.close()
     d_type = dvalues['meta']['clinical']['benign_malignant']
-    if randint(1,5) == 5:
+    if randint(1,5) == 5:   # 80/20 split
         d_type = "test/"+d_type
     #os.rename("thumbs/"+_id+".jpeg","thumbs/"+d_type+"/"+_id+".jpeg")
     if not os.path.isfile('thumbs/benign/'+_id+'.jpeg')\
      and not os.path.isfile('thumbs/malignant/'+_id+'.jpeg')\
      and not os.path.isfile('thumbs/test/benign/'+_id+'.jpeg')\
-     and not os.path.isfile('thumbs/test/malignant/'+_id+'.jpeg'):
-        copyfile("thumbs/"+_id+".jpeg","thumbs/"+d_type+"/"+_id+".jpeg")
+     and not os.path.isfile('thumbs/test/malignant/'+_id+'.jpeg')\
+     and not d_type is None:
+        img = cv2.imread("thumbs/"+_id+".jpeg",0)
+        img = cv2.resize(img,(img_dim,img_dim))
+        cv2.imwrite("thumbs/"+d_type+"/"+_id+".jpeg",img)
+        #copyfile("thumbs/"+_id+".jpeg","thumbs/"+d_type+"/"+_id+".jpeg")
 
--- get_img_list.sh
+++ get_img_list.sh
@@ -1,2 +1,2 @@
-curl -X GET --header 'Accept: application/json' 'https://isic-archive.com/api/v1/image?limit=2000&sort=name&sortdir=1&detail=false' -o imgs_list.txt
+curl -X GET --header 'Accept: application/json' 'https://isic-archive.com/api/v1/image?limit=2000&sort=name&sortdir=-1&detail=false' -o imgs_list.txt
 
